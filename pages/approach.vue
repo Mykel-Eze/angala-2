074ab79c42b6a6640dbb71b98a93f4a9b2a950fe
Mykel-Eze <michaeleze1997@gmail.com>
2023-01-16 23:20:45 +0100

    Approach Page done

---
+++ pages/approach.vue
@@ -0,0 +1,220 @@
+<template>
+  <div id="approach-page">
+    <!---=== Home Section ===--->
+    <section id="home">
+      <div class="row full-height">
+        <div class="container full-height">
+          <div class="home-content-wrapper">
+            <div class="home-txt-side">
+              <h4 class="home-title">
+                <div class="small-title-desc sec-color">
+                  Our Approach
+                </div>
+                Integrated <br> network
+              </h4>
+              <div class="home-desc white-txt">
+                <p>
+                  Angala Fintech is a core part of a network of nine standalone solutions providers, each with the
+                  common purpose of supporting communities and enabling farmers, food producers and SMEs to
+                  increase output.
+                </p>
+                <p>
+                  The sister businesses interconnect and synergise to enable Angala Fintech to optimise its services.
+                </p>
+              </div>
+            </div>
+            <div class="home-img-side">
+              <img src="~/assets/images/integrated-network-img.png" alt="integrated-network" class="home-img">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Home Section ===--->
+
+    <!---=== Dynamic-Partnerships Section ===--->
+    <section id="dynamic-partnerships">
+      <div class="row">
+        <div class="container">
+          <div class="dp-wrapper">
+            <div class="dp-block">
+              <div class="dp-block-txt">
+                <p>
+                  Our network of solutions providers operates across agriculture, crop services, food and retail,
+                  energy, logistics, asset finance, and insurance. Angala Fintech, for example, provides financial
+                  services to smallholder farmers, with agents concentrated in agricultural regions. These agents
+                  interconnect with the other companies to provide any financial transactions the farmers require.
+                  An agent might work with Japtini Energy, for example, to provide loans for a solar irrigation pump.
+                </p>
+                <p>
+                  The Payrail offering starts with farming businesses and runs through the processed food
+                  distribution and retail networks. We take advantage of this natural ecosystem to keep abreast
+                  of demand for various financial services and respond with agents and training as needed.
+                </p>
+              </div>
+              <div class="dp-block-img">
+                <img src="~/assets/images/hand-shake.png" alt="hand-shake" class="hand-shake-img">
+              </div>
+            </div>
+            <div class="dp-block">
+              <div class="dp-block-big-txt">
+                <div class="flex-div pry-color">
+                  <img src="~/assets/images/recycle-icon.svg" alt="recycle-icon">
+                  <span>Together, we build dynamic partnerships for mutual problem solving.</span>
+                </div>
+              </div>
+              <div class="dp-block-img">
+                <img src="~/assets/images/ropes.png" alt="knotted ropes" class="knot-ropes-img">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Dynamic-Partnerships Section ===--->
+
+    <!---=== Angala-Companies Section ===--->
+    <section id="angala-companies">
+      <div class="row">
+        <div class="container">
+          <div id="arila-block-1" class="company-block-wrapper visible-xs-sm">
+            <div class="center">
+              <img src="~/assets/images/angala-fintech.svg" alt="angala-fintech" class="top-company-logo">
+            </div>
+            <div class="company-block">
+              <img src="~/assets/images/arila-white.png" alt="Arila" class="company-image">
+              <div class="company-desc">
+                <span class="arila-txt">Arila</span> connects, collaborates and coordinates between all of the
+                operating companies’ services, providing planning and support. Additionally, Arila provides
+                expertise in finance, technology, regulations, human resources and marketing.
+              </div>
+            </div>
+          </div>
+          <div class="angala-companies-wrapper">
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/alluvial.png" alt="alluvial" class="company-image">
+                <div class="company-name">
+                  Farming services
+                </div>
+                <div class="company-desc">
+                  Alluvial Agriculture provides training, technology, land preparation, irrigation, input
+                  supplies and market access within contiguous farmlands in community blocks
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/fadhila.png" alt="fadhila" class="company-image">
+                <div class="company-name">
+                  Crop protection & nutrients
+                </div>
+                <div class="company-desc">
+                  Fadhila designs crop protection and assesses crop nutrient requirements, and then commissions
+                  producers to provide quality inputs to the specifications of the farmers
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/japtini-food.png" alt="japtini-food" class="company-image">
+                <div class="company-name">
+                  Japtini Food & Retail
+                </div>
+                <div class="company-desc">
+                  Japtini Food & Retail aggregates the primary produce and takes it to the marketplace
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper jl">
+              <div class="company-block">
+                <img src="~/assets/images/japtini-energy.png" alt="japtini-energy" class="company-image">
+                <div class="company-name">
+                  Japtini Energy
+                </div>
+                <div class="company-desc">
+                  Japtini Energy provides solar panels and batteries for affordable and reliable power
+                </div>
+              </div>
+            </div>
+            <div id="arila-block" class="company-block-wrapper hidden-xs-sm">
+              <div class="center">
+                <img src="~/assets/images/angala-fintech.svg" alt="angala-fintech" class="top-company-logo">
+              </div>
+              <div class="company-block">
+                <img src="~/assets/images/arila-white.png" alt="Arila" class="company-image">
+                <div class="company-desc">
+                  <span class="arila-txt">Arila</span> connects, collaborates and coordinates between all of the
+                  operating companies’ services, providing planning and support. Additionally, Arila provides
+                  expertise in finance, technology, regulations, human resources and marketing.
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper jl">
+              <div class="company-block">
+                <img src="~/assets/images/japtini-logistics.png" alt="japtini-logistics" class="company-image">
+                <div class="company-name">
+                  Cargo & Transport
+                </div>
+                <div class="company-desc">
+                  Japtini Logistics uses advanced technology to open access to third party logistics service providers
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/alluvial.png" alt="alluvial" class="company-image">
+                <div class="company-name">
+                  Insurance
+                </div>
+                <div class="company-desc">
+                  Japtini Insurtech uses technology to source and retain the best insurance policies
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/japtini-finance.png" alt="japtini-finance" class="company-image">
+                <div class="company-name">
+                  Asset Finance
+                </div>
+                <div class="company-desc">
+                  Japtini Asset Finance helps companies purchase equipment such as trucks or combine harvesters by
+                  working with financial institutions and service providers
+                </div>
+              </div>
+            </div>
+            <div class="company-block-wrapper">
+              <div class="company-block">
+                <img src="~/assets/images/angala-insuretech.png" alt="angala-insuretech" class="company-image">
+                <div class="company-name">
+                  Angala Insurtech
+                </div>
+                <div class="company-desc">
+                  Angala Insurtech uses technology to source and retain the best insurance policies
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!---=== end of Angala-Companies Section ===--->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ApproachPage'
+}
+</script>
+
+<style src="..\assets\css\home.css"></style>
+<style src="..\assets\css\approach.css"></style>
+
+<style scoped>
+#home {
+  background-image: url('../assets/images/hero-image-approach.jpg');
+  height: calc(100vh + 120px);
+}
+</style>
